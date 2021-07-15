b7fccf4289ef71c5487e7467df9df1eea5d4282f
Jacky Eastmoon <jacky_eastmoon@hotmail.com>
2021-07-15 16:35:31 +0800

    Fix build directory not initial

--- src/run-cmake.sh
+++ src/run-cmake.sh
@@ -1,5 +1,6 @@
 if [ -d ${1} ]; then
-    cmake -S ./${1} ./build/${1}
+    [ ! -d ./build/${1} ] && mkdir ./build/${1}
+    cmake -S ./${1} -B ./build/${1}
     make -C ./build/${1}
     echo ">> Execute example"
     echo ""
